f030093a70b4c65272541a89e6fd25c566b179a8
eve-atieno <atienoevelyne53@gmail.com>
2023-02-19 22:48:44 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, sex, age) VALUES
+('Mr. Chocolate', 'M', 5),
+('Rowdy', 'M', 8),
+('Tabitha', 'F', 3),
+('Sergeant Brown', 'M', 6),
+('Melissa', 'F', 4),
+('Grinch', 'M', 2),
+('Wendy', 'F', 7),
+(NULL, 'M', 1);
